2d8ad422d5c4a24f70855804e8a8e64df187191f
whddus426 <whddus426@gmail.com>
2024-03-06 23:46:35 +0900

    서블릿으로 구구단 구현

--- src/main/java/com/sbs/jsp/board/servlet/GugudanServlet.java
+++ src/main/java/com/sbs/jsp/board/servlet/GugudanServlet.java
@@ -8,14 +8,20 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/hello")
-public class HelloServlet extends HttpServlet {
+@WebServlet("/gugudan")
+public class GugudanServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     req.setCharacterEncoding("UTF-8"); // 들어오는 데이터를  UTF-8로 해석하겠다.
     resp.setCharacterEncoding("UTF-8"); // 완성되는 HTML의 인코딩을 UTF-8로 하겠다.
     resp.setContentType("text/html; charset=utf-8"); // 브라우저에게 우리가 만든 결과물이 UTF-8이다 라고 말하는 의미
 
-    resp.getWriter().append("안녕하세요1231."); //반응+추가
+    int dan = Integer.parseInt(req.getParameter("dan"));
+
+    resp.getWriter().append("<h1>%d단</h1>\n".formatted(dan));
+
+    for (int i = 1; i <= 9; i++) {
+      resp.getWriter().append("<div>%d * %d = %d</div>\n".formatted(dan,i, dan * i));
+    }
   }
 }
