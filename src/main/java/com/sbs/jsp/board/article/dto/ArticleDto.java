21d30a390ee2733d760c5b297471c4bf38d92dea
whddus426 <whddus426@gmail.com>
2024-03-07 21:08:12 +0900

    게시물 3개 노출

---
+++ src/main/java/com/sbs/jsp/board/article/dto/ArticleDto.java
@@ -0,0 +1,14 @@
+package com.sbs.jsp.board.article.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ArticleDto {
+  private long id;
+  private String title;
+  private String body;
+}
