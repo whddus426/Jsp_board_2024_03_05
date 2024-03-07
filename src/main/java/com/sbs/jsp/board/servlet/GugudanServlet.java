0c02ead1ee391d51d731d5e56bee775a52b8074a
whddus426 <whddus426@gmail.com>
2024-03-07 18:42:59 +0900

    req와 resp를 직접 다루기 귀찮으니, Rq를 이용해서 문제 해결

--- src/main/java/com/sbs/jsp/board/servlet/GugudanServlet.java
+++ src/main/java/com/sbs/jsp/board/servlet/GugudanServlet.java
@@ -1,5 +1,6 @@
 package com.sbs.jsp.board.servlet;
 
+import com.sbs.jsp.board.Rq;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,16 +13,15 @@ import java.io.IOException;
 public class GugudanServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    req.setCharacterEncoding("UTF-8"); // 들어오는 데이터를  UTF-8로 해석하겠다.
-    resp.setCharacterEncoding("UTF-8"); // 완성되는 HTML의 인코딩을 UTF-8로 하겠다.
-    resp.setContentType("text/html; charset=utf-8"); // 브라우저에게 우리가 만든 결과물이 UTF-8이다 라고 말하는 의미
+    Rq rq = new Rq(req, resp);
 
-    int dan = Integer.parseInt(req.getParameter("dan"));
+    int dan = rq.getIntParam("dan",9);
+    int limit = rq.getIntParam("limit",9);
 
-    resp.getWriter().append("<h1>%d단</h1>\n".formatted(dan));
+    rq.appendBody("<h1>%d단</h1>\n".formatted(dan));
 
-    for (int i = 1; i <= 9; i++) {
-      resp.getWriter().append("<div>%d * %d = %d</div>\n".formatted(dan,i, dan * i));
+    for (int i = 1; i <= limit; i++) {
+      rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan,i, dan * i));
     }
   }
 }
